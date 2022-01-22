1d4e09e519f126537bf30b5a1c58ea40d4a1b07f
Sean Dezoysa <sean.dezoysa@equipmentshare.com>
2022-01-22 10:13:46 -0500

    add remaining steps

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const puppeteer = require('puppeteer');
+const Chance = require('chance');
+const path = require('path');
+
+const chance = new Chance();
 
 /**
  * example usage: `node script.js 120`
@@ -6,7 +10,7 @@ const puppeteer = require('puppeteer');
  * In this case the value is 120. 
  * The 1 year list results should then remove all captured records with a "days on zillow value of less than 120"
  */
-const lowestDaysAllow = process.argv[2]
+const lowestDaysAllowed = process.argv[2] ?? 120
 
 // This is the first page to start the scrape on
 const targetPage1 = process.argv[3]
@@ -22,10 +26,98 @@ const targetPage1 = process.argv[3]
     devtools: true,
   })
   
-  const page = await browser.newPage()
-  
   try {
+
+    const page = await browser.newPage()
+    await page.waitForSelector('#wrapper')
+    
     await page.goto(targetPage1)
+    const homeListingPages = []
+    const filteredListings = []
+
+
+    let nextButtonIsStillEnabled = true;
+
+    // for a single page, get all the listing data
+    // save in memory
+
+
+    // data gathering step --------------------
+    while( nextButtonIsStillEnabled ) {
+      const delayBetweenPageLoads = chance.normal({
+        mean: 10.78 * 1000,
+        dev: 6.21 * 1000,
+
+      })
+
+      await page.waitForTimeout(delayBetweenPageLoads)
+
+      const outgoingPageNextButton = page.$('#next')
+      await page.click(outgoingPageNextButton)
+
+      for ( let i = 0; i < homeListingPages.length; i++ ) {
+        await page.click(homeListingPages[i].url)
+  
+        const safeListingPageReadingTime = chance.normal({
+          mean: 10.32 * 1000,
+          dev: 5.12 * 1000,
+        })
+  
+        await page.waitForTimeout( safeListingPageReadingTime )
+  
+        // TODO fix all the data selectors to the real ones
+        const dataSection = page.$('#data')
+  
+        const daysOnZillow = Number ( dataSection.$('daysOnZillow') )
+
+        // filtering step --------------------------
+        if (daysOnZillow >= lowestDaysAllowed) {
+          const listingData = {
+            beds: dataSection.$('#beds'),
+            beds: dataSection.$('#baths'),
+            squareFeet: dataSection.$('#baths'),
+            address1: dataSection.$('#baths'),
+            cityStateZip: dataSection.$('#baths'),
+            daysOnZillow,
+          }
+    
+          
+          filteredListings.push(listingData);  
+        }
+  
+        // TODO update the next button state property
+        nextButtonIsStillEnabled = (await page.$('#next')).getProperties()
+          .disabled === true
+      }
+    }
+
+    browser.close();
+
+    // file saving step -------------------------------
+    const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+    const csvWriter = createCsvWriter({
+      path: separatedFilePath,
+      header: [
+        { id: 'daysOnZillow', title: 'daysOnZillow' },
+        { id: 'address1', title: 'address1' },
+        { id: 'cityStateZip', title: 'cityStateZip' },
+        { id: 'beds', title: 'beds' },
+        { id: 'baths', title: 'baths' },
+        { id: 'squarefeet', title: 'squarefeet' },
+      ]
+    })
+
+    const filePath = path.join( __dirname, 'nearExpiredListings.csv' )
+
+    csvWriter
+      .writeRecords( filteredListings )
+        .then(() => {
+          console.log( `successfully wrote to file ${filePath}` )
+        })
+        .catch( e => { 
+          throw new Error(e) 
+        } )
 
   }
   catch(e) {
