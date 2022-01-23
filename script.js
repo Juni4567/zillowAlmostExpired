8cfecc37eca5d588facf08adc0772b7446c72d18
Sean Dezoysa <sean.dezoysa@equipmentshare.com>
2022-01-22 19:38:38 -0500

    add variable to remove records with no days on zillow if set to true

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const lowestDaysAllowed = process.argv[2] ?? 120
 const targetPage1 = process.argv[3]
   ?? 'https://www.zillow.com/lucas-county-oh/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22Lucas%20County%2C%20OH%22%2C%22mapBounds%22%3A%7B%22west%22%3A-84.05532402050781%2C%22east%22%3A-82.82348197949219%2C%22south%22%3A41.185451773707626%2C%22north%22%3A42.1853634969627%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A616%2C%22regionType%22%3A4%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22doz%22%3A%7B%22value%22%3A%2290%22%7D%2C%22sort%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22price%22%3A%7B%22max%22%3A150000%7D%2C%22mp%22%3A%7B%22max%22%3A531%7D%7D%2C%22isListVisible%22%3Atrue%7D';
 
+const removeNoDaysOnZillowRecords = Boolean( process.argv[4] ) 
 
 // scraping starts ----------------------------
 
